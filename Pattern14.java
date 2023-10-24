56600250d9654ca28c83600999f763430987387b
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:20:52 +0530

    Create Pattern14.java

---
+++ Pattern14.java
@@ -0,0 +1,15 @@
+1.	public class Pattern14  
+2.	{  
+3.	public static void main(String[] args)   
+4.	{  
+5.	int i, j, rows=9;  
+6.	for (i = rows; i >= 1; i--)   
+7.	{  
+8.	for (j = 1; j <= i; j++)  
+9.	{  
+10.	System.out.print(j+" ");  
+11.	}  
+12.	System.out.println();  
+13.	}  
+14.	}  
+15.	}  

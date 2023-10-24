0ec12bc0955f7743817a574ce44415fc3d75a5de
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:15:11 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,22 @@
+1.	public class Pattern1  
+2.	{  
+3.	public static void main(String args[])   
+4.	{   
+5.	int i, j,number, n=7;   
+6.	//loop for rows  
+7.	for(i=0; i<n; i++)  
+8.	{   
+9.	number=1;   
+10.	//loop for columns  
+11.	for(j=0; j<=i; j++)  
+12.	{   
+13.	//prints num  
+14.	System.out.print(number+ " ");   
+15.	//incrementing the value of number   
+16.	number++;   
+17.	}   
+18.	//throws the cursor at the next line after printing each row  
+19.	System.out.println();   
+20.	}   
+21.	}   
+22.	}  

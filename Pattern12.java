7167f4fb3a035b58d1f23db55fd3671d6a4e69fb
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:20:30 +0530

    Create Pattern12.java

---
+++ Pattern12.java
@@ -0,0 +1,15 @@
+1.	public class Pattern12  
+2.	{  
+3.	public static void main(String[] args)   
+4.	{  
+5.	int rows=9;  
+6.	for (int i = 1; i <= rows; i++)   
+7.	{   
+8.	for (int j = rows; j >= i; j--)  
+9.	{  
+10.	System.out.print(j+" ");  
+11.	}  
+12.	System.out.println();  
+13.	}  
+14.	}  
+15.	}  

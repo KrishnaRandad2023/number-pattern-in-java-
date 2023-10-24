44751b6298374259ac2e37d5a360b8e7abc8d503
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:15:36 +0530

    Create Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+1.	public class Pattern2  
+2.	{              
+3.	public static void main(String[] args)   
+4.	{  
+5.	int i, j, k = 1;  
+6.	//inner loop  
+7.	for (i = 1; i <= 7; i++)   
+8.	{  
+9.	//outer loop  
+10.	for (j = 1; j< i + 1; j++)   
+11.	{  
+12.	//prints the value of k  
+13.	System.out.print(k++ + " ");  
+14.	}  
+15.	//throws the cursor at the next line  
+16.	System.out.println();  
+17.	}  
+18.	}  
+19.	}  

33ed0774e7c5e7be6c90928974ea652442ad4bde
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:16:28 +0530

    Create Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,40 @@
+1.	public class Pattern4  
+2.	{              
+3.	public static void main(String[] args)   
+4.	{  
+5.	for (int i = 1; i <= 4; i++)  
+6.	{  
+7.	int n = 8;  
+8.	for (int j = 1; j<= n - i; j++)   
+9.	{   
+10.	System.out.print(" ");   
+11.	}   
+12.	for (int k = i; k >= 1; k--)  
+13.	{  
+14.	System.out.print(k);  
+15.	}  
+16.	for (int l = 2; l <= i; l++)   
+17.	{  
+18.	System.out.print(l);   
+19.	}   
+20.	System.out.println();   
+21.	}   
+22.	for (int i = 3; i >= 1; i--)  
+23.	{  
+24.	int n = 10;  
+25.	for (int j = 0; j<= n - i; j++)   
+26.	{  
+27.	System.out.print(" ");   
+28.	}   
+29.	for (int k = i; k >= 1; k--)  
+30.	{  
+31.	System.out.print(k);  
+32.	}  
+33.	for (int l = 2; l <= i; l++)  
+34.	{  
+35.	System.out.print(l);  
+36.	}  
+37.	System.out.println();  
+38.	}  
+39.	}  
+40.	}  

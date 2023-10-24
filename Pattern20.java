96ee28d0e3dad02bb18db01608505bbc5918da34
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:23:52 +0530

    Create Pattern20.java

---
+++ Pattern20.java
@@ -0,0 +1,19 @@
+1.	public class Pattern20  
+2.	{  
+3.	public static void main(String[] args)   
+4.	{  
+5.	int i, j, k, rows=9;  
+6.	for(i=1;i< rows+1 ;i++)  
+7.	{  
+8.	for(j=i; j < rows+1 ;j++)  
+9.	{  
+10.	System.out.print(j + " ");  
+11.	}  
+12.	for(k=1; k < i ;k++)  
+13.	{  
+14.	System.out.print(k + " ");  
+15.	}  
+16.	System.out.println();  
+17.	}  
+18.	}  
+19.	}  

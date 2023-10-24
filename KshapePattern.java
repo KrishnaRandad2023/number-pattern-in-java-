db316dff73c31282b933890e14377d9312763c87
KrishnaRandad2023 <135265106+KrishnaRandad2023@users.noreply.github.com>
2023-10-24 09:26:30 +0530

    Create KshapePattern.java

---
+++ KshapePattern.java
@@ -0,0 +1,24 @@
+1.	public class KshapePattern  
+2.	{  
+3.	public static void main(String[] args)  
+4.	{  
+5.	for (int i = 8; i >= 0; i--)  
+6.	{  
+7.	int alphabet = 65;  
+8.	for (int j = 0; j <= i; j++)  
+9.	{  
+10.	System.out.print((char) (alphabet + j) + " ");  
+11.	}  
+12.	System.out.println();  
+13.	}  
+14.	for (int i = 0; i<= 8; i++)  
+15.	{  
+16.	int alphabet = 65;  
+17.	for (int j = 0; j <= i; j++)  
+18.	{  
+19.	System.out.print((char) (alphabet + j) + " ");  
+20.	}  
+21.	System.out.println();  
+22.	}  
+23.	}  
+24.	}  
